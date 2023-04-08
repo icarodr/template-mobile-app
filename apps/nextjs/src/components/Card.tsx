3297aefe09dd0ba91112d1b578741e50c5ff8a1a
icarodr <it46009@gmail.com>
2023-04-08 14:55:40 -0300

    changes and add components

---
+++ apps/nextjs/src/components/Card.tsx
@@ -0,0 +1,11 @@
+const Search = () => {
+
+    return(
+        <div className="flex">
+            <input className="text-[#868686] rounded-full h-10 w-80 border-none ml-12 bg-[#E5E5E5]" placeholder="Pesquisar..." type="text"/>
+            <button className="bg-[#BCBCBC] text-black h-10 w-10 rounded-full"  type="submit">lupa</button>
+        </div>
+    )
+}
+
+export default Search;
